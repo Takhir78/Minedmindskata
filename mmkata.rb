6e3947779b691090d7862ec395657adedcfe136b
Takhir78 <t.salimov@gmail.com>
2017-01-15 21:42:52 -0500

    put 1 2 mined 4 minds and 7 ... I got rid of the actual number where there is word

--- mmkata.rb
+++ mmkata.rb
@@ -1,11 +1,15 @@
 def minedminds(n)
   (1..n).each do |i|
-    if i % 2 == 0 && i % 4 == 0
+    if i % 1 == 0 && i % 2 == 0 && i % 4 == 0 && i % 7 == 0
       puts 'minedminds'
+    elsif i % 1 == 0
+      puts 'mined'
     elsif i % 2 == 0
       puts 'mined'
     elsif i % 4 == 0
       puts 'minds mined'
+    elsif i % 7 == 0
+      puts '...'
     
     else
       puts i
