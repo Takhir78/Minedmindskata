41ce960d79b8e4b2307242b3408a1716f1c23d3d
Takhir78 <t.salimov@gmail.com>
2017-01-18 14:33:53 -0500

    deleted lines which starts with #

--- minedkata2.rb
+++ minedkata2.rb
@@ -1,18 +1,15 @@
 def minedmindskata(parameter1)
-	# (1..100).each do |parameter1|
-
+	
 	if parameter1 % 3 == 0 && parameter1 % 5 == 0
 	'MinedMinds'
 	elsif parameter1 % 3 == 0
 		'Mined'
 	elsif parameter1 % 5 == 0
 		'Minds'
-	# elsif parameter1 % 15 == 0
-	# 	'MinedMinds'
-
+	
 	else
 		parameter1 
 
-    # end
+    
 	end
 end
