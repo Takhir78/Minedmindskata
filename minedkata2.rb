793e195d9fa69613be0fcda7cbe10f6e6b2f4e81
Takhir78 <t.salimov@gmail.com>
2017-01-18 13:45:10 -0500

    Test minedkata2

---
+++ minedkata2.rb
@@ -0,0 +1,14 @@
+def minedmindskata(parameter1)
+	if parameter1 == 3
+		"Mined"
+	elsif parameter1 == 5
+		"Minds"
+	elsif parameter1 == 6
+		"MinedMinds"
+
+	else
+		parameter1 
+
+
+	end
+end
---
+++ test_minedkata2.rb
@@ -0,0 +1,31 @@
+require "minitest/autorun"
+require_relative "minedkata2.rb"
+
+
+class Testminedkata < Minitest::Test
+	def test_1_returns_1
+		assert_equal(1,1)
+
+	end
+
+	def test_function_returns_1
+		assert_equal(1,minedmindskata(1))
+	end
+
+	def test_function_returns_2
+		assert_equal(2,minedmindskata(2))
+	end
+
+	def test_function_3
+		assert_equal("Mined",minedmindskata(3))
+	end
+	
+	def test_function_returns_5
+		assert_equal("Minds",minedmindskata(5))
+	end
+
+	def test_function_returns_6
+		assert_equal("MinedMinds",minedmindskata(6))
+	end
+end
+
