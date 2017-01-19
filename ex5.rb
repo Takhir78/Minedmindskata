70af94fb0c487c98b97ae290f2154150068a9fd8
Takhir78 <t.salimov@gmail.com>
2017-01-19 00:02:46 -0500

    Exercise #5 is done

---
+++ ex5.rb
@@ -0,0 +1,10 @@
+puts "Let's talk about #{'Takhir'}."
+puts my_age = 39 # 2017-1978 huh too old :((
+puts "He's #{'6'} inches tall."
+puts "He's #{'200'} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{'green'} eyes and #{'black'} hair."
+puts "His teeth are usually #{'like pearl'} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{39}, #{6}, and #{200} I get #{39 + 6 + 200}."
