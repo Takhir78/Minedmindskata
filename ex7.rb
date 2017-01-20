7d6aca0b9ea567c20c55a5e2c4f722ea231c5599
Takhir78 <t.salimov@gmail.com>
2017-01-19 23:11:29 -0500

    Exercise #7 is done

---
+++ ex7.rb
@@ -0,0 +1,22 @@
+puts "Mary had a little lamb"
+puts "Its fleece was white as #{'snow'}" # The word "snow" is printed because it is betweensingle quote mark. And it is not a variable
+puts "And everywhere that Mary went."
+puts "." * 10 #This will print ten dots
+
+end1 = "C"
+end2 = "h" 
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# in the one line where print is puts also comes together
+
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
