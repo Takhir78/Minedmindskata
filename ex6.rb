773396304c74624736d3a6e24ec12157aa61512d
Takhir78 <t.salimov@gmail.com>
2017-01-19 22:25:30 -0500

    Example 6 is done

---
+++ ex6.rb
@@ -0,0 +1,45 @@
+
+types_of_computer = 2
+
+x = "There are #{types_of_computer} types of computers"
+ desktop = "desktop"
+ laptop = "laptop"
+
+ y = "The first one is a #{desktop} and the second one is a #{laptop}"
+
+ puts "I said: #{x}"
+ puts "Then I said: #{y}"
+
+ a = "The #{laptop} is more light ..."
+ b = "compare to #{desktop}"
+
+ puts a + b
+
+puts "BELOW IS THE EXAMPLE 6 "
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
+
+
+
+
+
+ 
