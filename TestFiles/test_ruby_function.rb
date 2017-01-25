a634466d4ad6933f868486910a502657605436e6
Takhir78 <t.salimov@gmail.com>
2017-01-25 11:36:05 -0500

    Test run ruby function

---
+++ TestFiles/test_ruby_function.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require_relative "ruby_function.rb"
+
+class TestAddition <Minitest::Test
+
+# def test_1_plus_1_returns_2
+# 	assert_equal(2,add(1,1))
+# end
+
+def test_2_plus_3_returns_5
+	assert_equal(5,add(2,3))
+
+end
+end
