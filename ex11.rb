24c478a02f49ddc6a74e47f4991e37eb584a6ebd
Takhir78 <t.salimov@gmail.com>
2017-01-24 23:17:19 -0500

    Exercise 11 is done

---
+++ ex11.rb
@@ -0,0 +1,19 @@
+# print "How old are you? "
+# age = gets.chomp
+# print "How tall are you? "
+# height = gets.chomp
+# print "How much do you weigh? "
+# weight = gets.chomp
+
+# puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+
+print "Where are you from?"
+country = gets.chomp
+print "When did you come to Pittsburgh?"
+date = gets.chomp
+print "Who is your favorite team"
+team = gets.chomp
+
+puts "So, Mr.Takhir you are from #{country}, you came to Pittsburgh in #{date} and your favorite team is #{team}."
+
