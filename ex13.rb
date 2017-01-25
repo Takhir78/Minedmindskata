310505afb12f727f153272a939852f3e92b1108b
Takhir78 <t.salimov@gmail.com>
2017-01-25 00:49:46 -0500

    Exercise 13 is done

---
+++ ex13.rb
@@ -0,0 +1,13 @@
+# first, second, third = ARGV
+
+# puts "Your first variable is: #{first}"
+# puts "Your second variable is: #{second}"
+# puts "Your third variable is: #{third}"
+
+first, second, third, fourth, fifth = ARGV
+
+puts "Your first programming languageis: #{first}"
+puts "Your second country is: #{second}"
+puts "Your third team is: #{third}"
+puts "Your fourth power is: #{fourth}"
+puts "Your fifth t-shirt is: #{fifth}"
