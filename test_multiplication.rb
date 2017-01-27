388dc80a6ad7fbc8a8104453f6df7b2f52da7551
Takhir78 <t.salimov@gmail.com>
2017-01-27 13:50:44 -0500

    Winning Numbers

---
+++ test_multiplication.rb
@@ -0,0 +1,4 @@
+require "minitest/autorun"
+require_relative "multiplication.rb"
+
+class TestMultiplication < Minites::Test 
---
+++ test_winning_numbers.rb
@@ -0,0 +1,16 @@
+require "minitest/autorun"
+require_relative "winning_numbers"
+
+class TestWinningNumbers < Minitest::Test 
+
+	def test_1_return_true_if_number_is_in_array
+		results = winning_numbers(32830, [11829, 23891, 32830, 44983, 59203])
+		assert_equal(true, results)
+	end
+
+	def test_2_return_false_if_number_is_not_in_array
+		results = winning_numbers(32833, [11829, 23891, 32830, 44983, 59203])
+		assert_equal(false, results)
+	end
+
+end
