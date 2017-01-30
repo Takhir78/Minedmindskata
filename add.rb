d9f36b3b36477296c1868b31dff839f78c89fa20
Takhir78 <t.salimov@gmail.com>
2017-01-29 22:33:00 -0500

    Addition practice

--- add.rb
+++ add.rb
@@ -1,5 +1,5 @@
-def my_addition (num1, num2)
+def my_addition (num1, num2, num3)
 result = ""
-result = (num1 + num2)
+result = (num1 + num2 + num3)
 
 end
--- test_add.rb
+++ test_add.rb
@@ -2,16 +2,20 @@ require "minitest/autorun"
 require_relative "add.rb"
 
 class TestAddition < Minitest::Test
-	def test_0_add_0_equals_0
-		assert_equal(0, my_addition(0,0))
-	end
+	# def test_0_add_0_equals_0
+	# 	assert_equal(0, my_addition(0,0))
+	# end
 
-	def test_1_add_1_equals_2
-		assert_equal(2, my_addition(1, 1))
-	end
+	# def test_1_add_1_equals_2
+	# 	assert_equal(2, my_addition(1, 1))
+	# end
 
-	def test_15_add_25_equals_40
-		assert_equal(40, my_addition(15, 25))
+	# def test_15_add_25_equals_40
+	# 	assert_equal(40, my_addition(15, 25))
+	# end
+
+	def test_5_add_8_add_9_equals_22
+		assert_equal(22, my_addition(5, 8, 9))
 	end
 
 
