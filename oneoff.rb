b3970d1d8c23ae71d0ffb0f6f076b77c9d223576
Takhir78 <t.salimov@gmail.com>
2017-02-01 22:51:53 -0500

    OneOff NUMBER

--- oneoff.rb
+++ oneoff.rb
@@ -1,9 +1,42 @@
-# def oneoff
-# 	my_ticket = "1234"
-# 	winning_array = ["6666"]
+# In-class demonstration of one digit off (number & array)
+# Broken up into multiple methods
+# Marvin
 
-search_engines =
-  %w[Google Yahoo MSN].map do |engine|
-    "http://www." + engine.downcase + ".com"
-     end
+def off_by_one_number?(my_ticket, winning_ticket)
 
+	counter = 0
+	index = 0
+
+	my_ticket.length.times do
+
+		# puts winning_ticket[index]
+		# puts my_ticket[index]
+		if winning_ticket[index] == my_ticket[index]
+			counter += 1
+		# puts counter  # shows count of matching index pos/each iteration
+		end
+		index += 1
+
+	end
+
+	counter == my_ticket.length - 1  # returns true/false depending on condition
+
+end
+
+def winning_tickets_off_by_one(my_ticket, winning_tickets)
+
+	matches = []
+
+	winning_tickets.each do |winner|
+		if off_by_one_number?(winner, my_ticket)  # call previous method here
+												  # don't call this method from previous method
+												  # - hard to test without cascading
+			matches << winner  # shovel operator
+		end
+	end
+
+	return matches
+
+end
+
+# print winning_tickets_off_by_one("4567", ["4568", "2567", "4568", "5555", "9869"])
