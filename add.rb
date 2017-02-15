64cd9411a6d89feaf528f649ef7f10b78896dbec
Takhir78 <t.salimov@gmail.com>
2017-02-15 16:47:28 -0500

    pull

--- add.rb
+++ add.rb
@@ -1,5 +1,13 @@
 def my_addition (num1, num2, num3)
-result = ""
-result = (num1 + num2 + num3)
+	result = ""
+	result = (num1 + num2 + num3)
 
 end
+
+def my_addition_1 (num1, num2)
+	result = ""
+	result = (num1 + num2)
+
+end
+
+
--- test_add.rb
+++ test_add.rb
@@ -2,17 +2,17 @@ require "minitest/autorun"
 require_relative "add.rb"
 
 class TestAddition < Minitest::Test
-	# def test_0_add_0_equals_0
-	# 	assert_equal(0, my_addition(0,0))
-	# end
+	def test_3_add_5_equals_8
+		assert_equal(8, my_addition_1(3, 5))
+	end
 
-	# def test_1_add_1_equals_2
-	# 	assert_equal(2, my_addition(1, 1))
-	# end
+	def test_1_add_1_equals_2
+		assert_equal(7, my_addition(1, 1, 5))
+	end
 
-	# def test_15_add_25_equals_40
-	# 	assert_equal(40, my_addition(15, 25))
-	# end
+	def test_15_add_25_equals_40
+		assert_equal(40, my_addition(7, 25, 8))
+	end
 
 	def test_5_add_8_add_9_equals_22
 		assert_equal(22, my_addition(5, 8, 9))
